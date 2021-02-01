f66b5f566525d4d27d7e02b1b6af4d135b15267f
Amber Mattis <amber.mattis2@gmail.com>
2021-02-01 17:58:17 -0600

    practice

--- Codewars/codewars-practice.js
+++ Codewars/codewars-practice.js
@@ -69,6 +69,17 @@ console.log(lineBrake());
 console.log("")
 
 
+//Complete the solution so that it reverses the string passed into it.
+// Ex: 'world'  =>  'dlrow'
+
+function solution(str){
+    return str.split("").reverse().join("");
+}
+
+
+
+
+
 
 
 
