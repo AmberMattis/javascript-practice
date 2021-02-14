25cb3fbc522002816e89a0dd10ac4186bbeacda4
Amber Mattis <amber.mattis2@gmail.com>
2021-02-14 16:57:13 -0600

    js practice objects

--- objects/object-basics.js
+++ objects/object-basics.js
@@ -1,8 +1,29 @@
 //Objects are variables or properties that can contain multiple values.
-
 var person = {
     firstName: "Jane",
     lastName: "Doe"
 }
 
 console.log(person.lastName);
+
+
+//There are 2 ways to access object properties
+//1//
+// objectName.propertyName;
+//2//
+// objectName["propertyNamy\e"];
+
+
+
+
+
+//Example of methods inside of objects
+var personTwo = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function() {
+        return this.firstName + " " + this.lastName
+    }
+}
+
+console.log(personTwo.fullName());
